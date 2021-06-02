d5b4d536e923fdd9d882da1b498b9e859da2201d
Stephen Scotti <sjscotti@gmail.com>
2021-06-02 12:55:59 -0400

    debug1

--- src/bot.js
+++ src/bot.js
@@ -121,7 +121,8 @@ const bot = async ({
     keyFilename: join(process.cwd(), settings.PROJECT_JSON)
     // above was path.join(__dirname, settings.PROJECT_JSON)
   }); // was sessionClient in getDefaultCommand.js
-
+  // eslint-disable-next-line no-console -- temp
+  console.log('keyFilename', join(process.cwd(), settings.PROJECT_JSON));
   const {
     // The token of your bot -
     // https://discordapp.com/developers/applications/me
