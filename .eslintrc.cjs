bddf959ec6b5d9e5c5dd4b32314ae9fd52120703
Brett Zamir <brettz9@yahoo.com>
2021-06-02 10:27:46 +0800

    Revert "lint controls"
    This reverts commit ed8f4409acbf0fbcc5335122b47bbe29d22f3575.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,90 @@
+'use strict';
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
+  extends: [
+    'ash-nazg/sauron-node-overrides',
+    'plugin:chai-friendly/recommended'
+  ],
+  env: {
+    node: false,
+    browser: false,
+    'shared-node-browser': true
+  },
+
+  parser: '@babel/eslint-parser',
+
+  // Auto-set dynamically by config but needs to be explicit for Atom
+  parserOptions: {
+    ecmaVersion: 2021
+  },
+
+  overrides: [
+    {
+      files: '**/*.md/*.js',
+      globals: {
+        doAIProcessing: 'readonly',
+        internalBotResponseListener: 'readonly',
+        internalBotErrorListener: 'readonly',
+
+        DiscordConstants: 'readonly',
+        getLocalizedSetting: 'readonly',
+        greets: 'readonly',
+        checkinGuilds: 'writeable',
+
+        message: 'readonly',
+        bahaibot: 'readonly',
+        buildBahaiBot: 'readonly',
+        checkins: 'readonly',
+        locales: 'readonly',
+        fetch: 'readonly',
+        i18n: 'readonly',
+        striptags: 'readonly',
+        Discord: 'readonly',
+        client: 'readonly',
+        apiai: 'readonly',
+        fs: 'readonly',
+        getSettings: 'readonly',
+        getSettingsPath: 'readonly'
+      },
+      rules: {
+        'no-shadow': 0,
+        'jsdoc/require-jsdoc': 0,
+        'import/unambiguous': 0,
+        'import/no-unresolved': ['error', {
+          ignore: ['bahaibot']
+        }],
+        'no-unused-vars': ['error', {
+          varsIgnorePattern: 'commands|buildBahaiBot|bahaibot',
+          argsIgnorePattern: 'message'
+        }]
+      }
     }
+  ],
+
+  rules: {
+    'prefer-template': ['error'],
+
+    // Can remove this when to-dos are done
+    'unicorn/expiring-todo-comments': ['warn', {
+      allowWarningComments: false, terms: ['todo']
+    }],
+
+    'jsdoc/require-jsdoc': ['error', {
+      exemptEmptyFunctions: true,
+      require: {
+        ArrowFunctionExpression: true,
+        ClassDeclaration: true,
+        ClassExpression: true,
+        FunctionDeclaration: true,
+        FunctionExpression: true,
+        MethodDefinition: true
+      }
+    }],
+
+    // Browser-only
+    'compat/compat': 0,
+
+    // Allow for extensibility/clarity
+    'no-lonely-if': 0
+  }
 };
