c3428408e70d6fe3d212052f88a364be4a58e3c9
Brett Zamir <brettz9@yahoo.com>
2025-10-20 10:40:02 -0700

    fix(demo): complete non-functional skeleton

--- public/index.js
+++ public/index.js
@@ -1,4 +1,41 @@
 import integratedClientServerBot from
   '../dist/integratedClientServerBot.esm.js';
 
-integratedClientServerBot({});
+integratedClientServerBot({
+  dialogflow: {
+    /**
+     *
+     */
+    SessionsClient: class {}
+  },
+  Discord: {
+    GatewayIntentBits: {
+      Guilds: null,
+      GuildMessages: null,
+      MessageContent: null,
+      GuildMembers: null,
+      GuildPresences: null
+    },
+    /**
+     *
+     */
+    Client: class {
+      /**
+       * @returns {void}
+       */
+      login () {
+        // eslint-disable-next-line no-console -- Debugging
+        console.log('logging in', this);
+      }
+      /**
+       * @param {Event} ev
+       * @param {() => void} handler
+       * @returns {void}
+       */
+      on (ev, handler) {
+        // eslint-disable-next-line no-console -- Debugging
+        console.log('Logging', ev, handler, this);
+      }
+    }
+  }
+});
